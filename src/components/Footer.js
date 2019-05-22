8ef0eca2b84dca53aeda9d0a5ae8764ef5d2d545
mattwelson <matthew.welson@gmail.com>
2019-05-22 16:10:29 +1200

    Adding links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,40 @@ const Footer = styled.footer`
   justify-content: center;
 `
 
+const StyledLink = styled.a`
+  &,
+  &:visited,
+  &:hover,
+  &:active {
+    color: ${({ color }) => color};
+  }
+`
+
 export default () => (
   <Footer>
-    <FontAwesomeIcon icon={['fab', 'strava']} size="2x" />
-    <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
-    <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
+    <StyledLink
+      href="https://www.strava.com/athletes/mattwelson"
+      target="_blank"
+      rel="noopener noreferrer"
+      color="#FF5722"
+    >
+      <FontAwesomeIcon icon={['fab', 'strava']} size="2x" />
+    </StyledLink>
+    <StyledLink
+      href="https://www.instagram.com/m.welson/"
+      target="_blank"
+      rel="noopener noreferrer"
+      color="#673AB7"
+    >
+      <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
+    </StyledLink>
+    <StyledLink
+      href="https://twitter.com/mattwelson"
+      target="_blank"
+      rel="noopener noreferrer"
+      color="#03A9F4"
+    >
+      <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
+    </StyledLink>
   </Footer>
 )
